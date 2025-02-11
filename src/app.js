c88da80be4963e065325b15abe9dd94089ca1aeb
vise07cs <vikram7cs@gmail.com>
2025-02-11 15:05:30 +0530

    using http methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,37 @@ const express=require("express");
 const app=express();
 const port=3001;
 
-app.get("/",(req,res)=>{
+app.get("/user",(req,res)=>{
   res.send("WELCOME TO THE LANDING PAGE")
 })
-app.get("/home",(req,res)=>{
-  res.send("WELCOME TO THE HOME PAGE !!!!!!!!!!")
+
+
+app.post("/user",(req,res)=>{
+  res.send({
+    "name":"vikram",
+    "Lname":"Singh"
+  })
 })
 
-app.get("/contact",(req,res)=>{
-  res.send("CONTACT US")
+
+
+app.delete("/user",(req,res)=>{
+  res.send("Data Deleted")
 })
+
+
+
+// app.get("/home",(req,res)=>{
+//   res.send("WELCOME TO THE HOME PAGE !!!!!!!!!!")
+// })
+
+// app.get("/contact",(req,res)=>{
+//   res.send("CONTACT US")
+// })
+
+
+
+
 
 app.listen(port,()=>{
   console.log("App running successfully on port 3001")
