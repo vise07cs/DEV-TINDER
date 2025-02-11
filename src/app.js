7cde8cd631dba142c336fe298b1cc1a978439a96
vise07cs <vikram7cs@gmail.com>
2025-02-11 18:48:12 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -2,23 +2,50 @@ const express=require("express");
 const app=express();
 const port=3001;
 
-app.get("/user",(req,res)=>{
-  res.send("WELCOME TO THE LANDING PAGE")
-})
 
 
-app.post("/user",(req,res)=>{
-  res.send({
-    "name":"vikram",
-    "Lname":"Singh"
-  })
-})
+app.get("/user",(req,res,next)=>{
+  console.log("route handler 01")
+  next();
+},
+(req,res,next)=>{
+console.log("route handler 02")
+ next();
+},
+((req,res)=>{
+  console.log("route handler 03")
+  res.send("route handler 03")
+}
 
 
 
-app.delete("/user",(req,res)=>{
-  res.send("Data Deleted")
-})
+)
+)
+
+
+
+// app.get("/user",(req,res)=>{
+//   res.send("WELCOME TO THE LANDING PAGE")
+// })
+
+// app.get('/users/:userId/books/:bookId',(req,res)=>{
+//   // res.send("WELCOME TO THE ABC PAGE")
+//   res.send(req.params)
+// })
+
+
+// app.post("/user",(req,res)=>{
+//   res.send({
+//     "name":"vikram",
+//     "Lname":"Singh"
+//   })
+// })
+
+
+
+// app.delete("/user",(req,res)=>{
+//   res.send("Data Deleted")
+// })
 
 
 
