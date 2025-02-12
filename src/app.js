a2b891ebc28bdea54138d51fe0924584188b8eb9
vise07cs <vikram7cs@gmail.com>
2025-02-12 16:04:53 +0530

    /signup api to add data to database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
 const express=require("express");
 const {connectDB}=require("./config/database.js")
+const User=require("./model/user.js")
 const app=express();
 let port=3001
+
+app.post("/signup",async (req,res)=>{
+  const user=new User({
+    firstName:"akshay",
+    lastName:"singh",
+    email:"a7cs@gmail.com",
+    age:30,
+    password:"akshay@1234"
+
+  })
+
+  try {
+    await user.save();
+    res.send("user added successfully")
+    console.log('user added successfully');
+  
+    
+  } catch (error) {
+    res.status(501).send("unable to add data ")
+    
+  }
+
+ 
+
+
+})
 
 connectDB().then(()=>{
   try {
