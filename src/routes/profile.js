58ae81167ca70b9d7909969b1e7cec6d37aab7ac
vise07cs <vikram7cs@gmail.com>
2025-02-22 12:28:25 +0530

    PATCH API: /edit/password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,10 @@
 const express=require("express");
 const profileRoute=express.Router();
+// const User=require("../model/user.js")
 const User=require("../model/user.js")
+const bcrypt = require('bcrypt');
+
+
 
 
 
@@ -23,15 +27,15 @@ profileRoute.get("/profile/view",userAuth, async (req,res)=>{
   }
   
 })
-
+// edit profile 
 profileRoute.patch("/profile/edit",userAuth,async (req,res)=>{
   try {
     if(!validateEditDetails(req)){
       throw new Error("Invalid Requesrt");
       
     }
-    console.log(req.body);
-    // const loggedInUser=req.user;
+    // console.log(req.body);
+    const loggedInUser=req.user;
 
     Object.keys(req.body).forEach((key)=>(loggedInUser[key]=req.body[key]))
     // console.log(loggedInUser)
@@ -44,7 +48,7 @@ profileRoute.patch("/profile/edit",userAuth,async (req,res)=>{
 
 
 
-    
+
   } catch (error) {
     console.log(error)
 
@@ -52,8 +56,51 @@ profileRoute.patch("/profile/edit",userAuth,async (req,res)=>{
 
     
   }
+})
  
 
 
-} )
+
+
+
+profileRoute.patch("/profile/edit/password",userAuth,async (req,res)=>{
+
+   try {
+    
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+        return res.status(400).json({ message: "Both old and new passwords are required" });
+    }
+  
+     
+     const loggedInUser=req.user;
+ 
+     const passwordHash=loggedInUser.password;
+
+  
+     const isPasswordValid=await bcrypt.compare(oldPassword,passwordHash);
+
+  
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Incorrect old password" });
+  }
+  
+  const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+  await User.findByIdAndUpdate(loggedInUser._id, { password: hashedNewPassword });
+  
+  res.status(200).json({ message: "Password updated successfully" });
+    
+  
+
+    
+}catch (error) {
+      console.log(error)
+
+    res.status(501).send("something went wrong "+ error)
+
+    
+   }
+  })
+ 
 module.exports=profileRoute
